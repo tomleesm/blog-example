0200a6b7deb42ab9f739badc35d2fc3b13f1edbe
Tom <tomleesm@gmail.com>
2024-01-18 20:50:51 +0800

    安裝 jQuery

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -5,6 +5,9 @@
  */
 
 import axios from 'axios';
+import jQuery from 'jquery';
+
+window.$ = jQuery;
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
