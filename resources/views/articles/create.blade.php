004c1a9330ccae73bd70b7eb5fcecd53ff868b98
Tom <tomleesm@gmail.com>
2024-02-03 11:52:59 +0800

    新增的表單可以在驗證錯誤時填上之前的輸入

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -8,7 +8,7 @@
     
     <div>
       <label for="article_title">Title</label><br>
-      <input type="text" name="article[title]" id="article_title">
+      <input type="text" name="article[title]" id="article_title" value="{{ old('article.title') }}">
       @foreach($errors->get('article.title') as $message)
         <div>{{ $message }}</div>
       @endforeach
@@ -16,7 +16,7 @@
   
     <div>
       <label for="article_body">Body</label><br>
-      <textarea name="article[body]" id="article_body"></textarea><br>
+      <textarea name="article[body]" id="article_body">{{ old('article.body') }}</textarea><br>
       @foreach($errors->get('article.body') as $message)
         <div>{{ $message }}</div>
       @endforeach
