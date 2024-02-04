f41624bf51cd2bc4f4277569661f540827c1077c
Tom <tomleesm@gmail.com>
2024-02-04 11:36:34 +0800

    修正 csrf token in <meta>

--- resources/views/layouts/application.blade.php
+++ resources/views/layouts/application.blade.php
@@ -3,7 +3,7 @@
   <head>
     <title>Blog example</title>
     <meta name="viewport" content="width=device-width,initial-scale=1">
-    <meta name="csrf_token" content="{{ csrf_token() }}">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
   </head>
 
   <body>
