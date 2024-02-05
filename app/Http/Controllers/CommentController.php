9dfe658a4150c952eab9f48c6de436647641fe4f
Tom <tomleesm@gmail.com>
2024-02-05 10:55:50 +0800

    CommentController 新增 store() 新增評論

---
+++ app/Http/Controllers/CommentController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Article;
+use App\Models\Comment;
+
+class CommentController extends Controller
+{
+    public function store($articleId, Request $request)
+    {
+        $article = Article::find($articleId);
+        $comment = new Comment();
+        $comment->commenter = $request->input('comment.commenter');
+        $comment->body = $request->input('comment.body');
+
+        if ($article->comments()->save($comment)) {
+            return redirect()->route('articles.show', ['article' => $article->id]);
+        } else {
+            throw new \Exception('it can not store a new article');
+        }
+    }
+}
