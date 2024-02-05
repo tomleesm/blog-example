2b28ea09568f5b5d77b4e0257b484b5ce8b2f447
Tom <tomleesm@gmail.com>
2024-02-05 13:26:56 +0800

    不顯示 status 為 archived 的評論

--- resources/views/comments/comment.blade.php
+++ resources/views/comments/comment.blade.php
@@ -1,9 +1,11 @@
-<p>
-  <strong>Commenter:</strong>
-  {{ $comment->commenter }}
-</p>
+@unless($comment->isArchived())
+  <p>
+    <strong>Commenter:</strong>
+    {{ $comment->commenter }}
+  </p>
 
-<p>
-  <strong>Comment:</strong>
-  {{ $comment->body }}
-</p>
+  <p>
+    <strong>Comment:</strong>
+    {{ $comment->body }}
+  </p>
+@endunless
