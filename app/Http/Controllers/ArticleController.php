d7cab8fa1f99a57f54f9a9b12f7f8bf21a08d917
Tom <tomleesm@gmail.com>
2024-06-14 00:38:10 +0800

    ArticleController 新增 store()，並用 StoreArticleRequest 驗證表單

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -3,11 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\StoreArticleRequest;
 
 class ArticleController extends Controller
 {
     public function index()
     {
         return view('articles.index');
+    }
+
+    public function store(StoreArticleRequest $request)
+    {
+        $validated = $request->validated();
+        return [ 'status' => 'OK' ];
     }
 }
